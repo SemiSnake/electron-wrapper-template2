963452029a7eaae56d6a92367cca062ecc4c9ecd
Zlommy <48889472+SemiSnake@users.noreply.github.com>
2024-09-13 11:16:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const os = require('os');
 const computerName = os.hostname
+console.log(computerName)
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
