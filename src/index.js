b2afb91ab8a888029879feb93ab90e4eb4300029
Zlommy <48889472+SemiSnake@users.noreply.github.com>
2024-09-16 11:43:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const os = require('os');
-const computerName = os.hostname
+const computerName = os.hostname()
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -25,7 +25,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 };
 function getComputerName () {
-  return "Hello!";
+  return computerName
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
