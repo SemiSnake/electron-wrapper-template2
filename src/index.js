a6b83155a8b552c74eac0143162448f5c988a28e
Zlommy <48889472+SemiSnake@users.noreply.github.com>
2024-09-24 04:57:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,11 +25,15 @@ const createWindow = () => {
 function getUserName () {
   return require("os").userInfo().username
 }
+function quitApplication () {
+  app.quit();
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   ipcMain.handle('usernameQuery', getUserName)
+  ipcMain.handle('quitQuery', quitApplication)
   createWindow()
 })
 
